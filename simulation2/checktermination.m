b07adfbf31dcf564a1909e453c5ac9b27401d3ac
sanghnjung <84378993+sanghnjung@users.noreply.github.com>
2023-03-30 07:25:42 +0900

    matlab_files_renewed

--- simulation2/checktermination.m
+++ simulation2/checktermination.m
@@ -1,7 +1,6 @@
 function running = checktermination(egoVehicleGridPosition,goalGridPosition)
     if egoVehicleGridPosition == goalGridPosition
         running = false;
-        disp(running);
     else
         running = true;
     end
--- simulation2/simulate.m
+++ simulation2/simulate.m
@@ -1,5 +1,6 @@
-function [scenario, running, reward, updatedRoadGrid, countGrid, distance] = simulate(scenario, rewardValues, gridsize, goalGridPosition, action, egoVehicleSpeed, staticRoadGrid)
+function [scenario, running, reward, updatedRoadGrid, countGrid] = simulate(scenario, rewardValues, gridsize, goalGridPosition, action, egoVehicleSpeed, staticRoadGrid)
     egoVehiclePosition = scenario.Actors(1).Position;
+
 
     % gridtarget = calculategridtarget(egoVehicleGridPosition, action, gridsize);
     newEgoVehiclePosition = egovehicleupdate(egoVehiclePosition, scenario.SampleTime, egoVehicleSpeed, action);
@@ -7,13 +8,16 @@ function [scenario, running, reward, updatedRoadGrid, countGrid, distance] = sim
 
     egoVehicleGridPosition = ceil(newEgoVehiclePosition/10);
     egoVehicleGridPosition = egoVehicleGridPosition(1:2);
-%     disp(egoVehicleGridPosition)
 
+    [goalGridPositionX, goalGridPositionY] = find(staticRoadGrid==30);
+    goalGridPosition_flipped = [goalGridPositionX, goalGridPositionY];
     temp = advance(scenario);
 
     [countGrid,updatedRoadGrid] = calculategrid(scenario, staticRoadGrid, gridsize, egoVehicleGridPosition);
+
     running = checktermination(egoVehicleGridPosition,goalGridPosition);
-    [reward,distance] = calculatereward(newEgoVehiclePosition,staticRoadGrid, countGrid, egoVehicleGridPosition, rewardValues, goalGridPosition, running);
+    reward = calculatereward(newEgoVehiclePosition,staticRoadGrid, countGrid, egoVehicleGridPosition, rewardValues, goalGridPosition_flipped, running, egoVehiclePosition);
+
 
 
 
